71cf860fe7709a43a8058e8caa32d945b26f84b8
lisa <lisasoders@gmail.com>
2021-11-29 11:39:49 +0100

    done

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const kostnadsLista = [];
 const tabell = [];
 
 
-// Gör att det som knappen får fram stannar kvar
+
 function count(e) {
-    e.preventDefault();
+    e.preventDefault();  // Förhindrar att värdet efter man klickat på knappen försvinner
 
     let utgiftSumma = 0;
     let inkomstSumma = 0;
@@ -26,44 +26,47 @@ function count(e) {
     
     console.log(nummer);
 
+    // När man skriver i "beskrivning" och "nummer" samt väljer "+" kommer värdena pushas till inkomstLista
+
     if(select.value == "+") {
 
         document.querySelector(".inkomster").innerHTML +=`<li class="punktBesk"> ${beskrivning} </li>`
         document.querySelector(".inkomster").innerHTML +=`<li class="punktNum"> ${nummer} Kr</li>`
-        document.querySelector(".inkomster").style.animation = "para 1s";
+        document.querySelector(".inkomster").style.animation = "para 1s"; // Animationen kopplad till inkomstrutan
+       
         inkomstLista.push(nummer);
         
-
-       
-        
     }
+    
+    // // När man skriver i "beskrivning" och "nummer" samt väljer "-" kommer värdena pushas till kostnadsLista
 
    else if(select.value == "-") {
         document.querySelector(".utgifter").innerHTML +=`<li class="punktBesk"> ${beskrivning}</li>`
         document.querySelector(".utgifter").innerHTML +=`<li class="punktNum"> ${nummer} Kr</li>`
-        document.querySelector(".utgifter").style.animation = "nopara 2s";
+        document.querySelector(".utgifter").style.animation = "nopara 2s"; // Animation kopplad till utgiftslistan
 
         kostnadsLista.push(nummer);
         
     }
 
-    
+    // Tar in värden från inkomstLista och ger värden till inkomstSumma
     inkomstLista.map( (e)=> {
  
         console.log(parseFloat(e))
         inkomstSumma += parseFloat(e)
     })
-    // console.log(inkomstSumma);
+    
+    // Tar in värden från kostnadsLista och ger värden till utgiftSumma
     kostnadsLista.map( (e)=> {
  
         console.log(parseFloat(e))
         utgiftSumma += parseFloat(e)
     })
-    //console.log(utgiftSumma);
+   
 
-    document.querySelector(".summa").textContent = inkomstSumma - utgiftSumma + ' Kr';
-    document.querySelector(".besk").value = "";
-    document.querySelector(".num").value = "";
+    document.querySelector(".summa").textContent = inkomstSumma - utgiftSumma + ' Kr'; 
+    document.querySelector(".besk").value = ""; // Nollställer inputfält "beskrivning" efter man klickat på knappen
+    document.querySelector(".num").value = ""; // Nollställer inputfält "kr" efter man klickat på knappen
 
 }
 
